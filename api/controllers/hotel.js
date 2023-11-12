6637318108feaef1ef14264ad592feda6d8d24ef
gbarberan23 <gabrielbv.2000@gmail.com>
2023-11-12 00:10:19 -0500

    commit

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -44,13 +44,17 @@ export const getHotel = async (req, res, next) => {
 }
 //////
 export const getHotels = async (req, res, next) => {
-    try{
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels);
-    }catch(err){
-        next(err)
+    const { min, max, ...others } = req.query;
+    try {
+      const hotels = await Hotel.find({
+        ...others,
+        cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+      }).limit(req.query.limit);
+      res.status(200).json(hotels);
+    } catch (err) {
+      next(err);
     }
-}
+  };
 
 export const countByCity = async (req, res, next) => {
     const cities = req.query.cities.split(",");
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,20 +1,22 @@
 import "./navbar.css"
+import { Link } from "react-router-dom";
 //import Assest from './assest/logo.png';
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        <div className="navContainer">
-        <a class="navbar-brand" routerLink="">
-        <span className="logo">Los Gavilanes</span>
-        </a>
-            <div className="navItem">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
-            </div>
-        </div>
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">lamabooking</span>
+        </Link>
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+     
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
